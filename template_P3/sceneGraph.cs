880388dd57d9ef9d4cd618ec9006e3fdff3022c8
Wootsz <wouterbrozius@hotmail.com>
2016-06-18 14:58:51 +0200

    Rasterizer

---
+++ template_P3/sceneGraph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using Template_P3;
+
+namespace template_P3
+{
+    public class SceneGraph
+    {
+        public SceneGraph()
+        {
+
+        }
+
+        public void Render(Matrix4 matrix, Mesh mesh)
+        {
+            Matrix4 newmatrix = matrix * mlocal
+            mesh.Render(newmatrix);
+            for(int i = 0; i < mesh.children.Count; i++)
+                SceneGraph.Render(newmatrix, mesh.children[0]);
+        }
+    }
+}
